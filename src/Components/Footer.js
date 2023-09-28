b6bbf8b3dd23a123fa80b98939d4f1660539066c
Wojtanello-coder <walonale@gmail.com>
2023-09-28 08:55:26 +0200

    liink

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,9 @@ function Footer(){
     return(
         <div id='footer'>
             stupka :33333333
-            <img src={logo}/>
+            <a href='http://ckziu.jaworzno.pl' target='_blank'>
+                <img src={logo}/>
+            </a>
         </div>
     )
 }
