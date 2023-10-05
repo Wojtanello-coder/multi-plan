e4a65f9096d5f8c406c8123c28ffb9610babb3b3
Wojtanello-coder <walonale@gmail.com>
2023-10-05 09:17:21 +0200

    attempt at plan

--- src/Components/Plan.js
+++ src/Components/Plan.js
@@ -1,9 +1,18 @@
 function Plan(props){
     console.log(props.data)
+    let table = [];
+    if(props.data.length!=0){
+        for (let i = 0; i < props.data.lesson.length; i++) {
+            let tds = props.data.lesson.slice(i*props.data.dayLength[0], i*props.data.dayLength[0]);
+            table.push(<tr> {tds} </tr>);
+        }
+    }
     return(
         <div id="plan">
             <table>
-                {}
+                <tbody>
+                    {table}
+                </tbody>
             </table>
         </div>
     )
